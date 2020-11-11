a9b000efd656377d1ce124db79cb895dc8ef44f2
hyliu <l372511387@163.com>
2020-11-11 22:40:04 +0800

    按照 python 代码规范更新了代码, 另外将 range 替换为了 tf.range 优化静态图的效率

--- 概率矩阵分解的简单实现/PMF.py
+++ 概率矩阵分解的简单实现/PMF.py
@@ -1,11 +1,14 @@
-import tensorflow as tf
 import numpy as np
-import sys
+import tensorflow as tf
+import sys as sys
+
 
 class PMF:
-    def __init__(self, userNum, itemNum, hNum, Activation = "sigmoid", lamdaU = 0.01, lamdaV = 0.01):
-        self.NormalInitializers = tf.keras.initializers.RandomNormal(mean= 0.0, stddev = 0.05)
-        self.regularizersL2 = tf.keras.regularizers.l2(l = 0.01)
+    def __init__(self, userNum, itemNum, hNum, Activation="sigmoid",
+                 lamdaU=0.01, lamdaV=0.01):
+        self.NormalInitializers = tf.keras.initializers.\
+            RandomNormal(mean=0.0, stddev=0.05)
+        self.regularizersL2 = tf.keras.regularizers.l2(l=0.01)
         self.optimizer = tf.keras.optimizers.Adam()
         self.Activation = "sigmoid"
         self.MSE = tf.keras.losses.MeanSquaredError()
@@ -25,23 +28,27 @@ class PMF:
         RMatrix = tf.cast(RMatrix, tf.float32)
         with tf.GradientTape() as tape:
             hatRMatrix = self.predict()
-            loss = self.MSE(RMatrix*IMatrix, hatRMatrix*IMatrix) + self.lamdaU * self.regularizersL2(self.U) + self.lamdaV * self.regularizersL2(self.V)
+            loss = self.MSE(RMatrix*IMatrix, hatRMatrix*IMatrix)\
+                + self.lamdaU * self.regularizersL2(self.U)\
+                + self.lamdaV * self.regularizersL2(self.V)
         trainParameter = [self.U, self.V]
         gradients = tape.gradient(loss, trainParameter)
         self.optimizer.apply_gradients(zip(gradients, trainParameter))
         return loss
 
     @tf.function
-    def fit(self, RMatrix, IMatrix, EpochNum = 2):
-        tf.print ("开始训练!")
-        for i in range(EpochNum):
+    def fit(self, RMatrix, IMatrix, EpochNum=2):
+        tf.print("开始训练!")
+        for i in tf.range(EpochNum):
             lossValue = self.trainStep(RMatrix, IMatrix)
-            tf.print ("Epoch: " + str(i) + "; loss: ", lossValue, output_stream=sys.stderr)
-        tf.print ("训练结束!")
+            tf.print("Epoch: ", i, "; loss: ",
+                     lossValue, output_stream=sys.stderr)
+        tf.print("训练结束!")
 
-RMatrix =np.load('R_train.npy')
-IMatrix =np.load('I_train.npy')
+
+RMatrix = np.load('R_train.npy')
+IMatrix = np.load('I_train.npy')
 userNum, itemNum = len(RMatrix), len(RMatrix[0])
 model = PMF(userNum, itemNum, 30)
 model.fit(RMatrix, IMatrix, 1000)
-print (model.predict())
+print(model.predict())
